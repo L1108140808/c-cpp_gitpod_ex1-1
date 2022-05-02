56c93a0f8f52638570815802ea90d35c0c6f2270
L1108140808 <104473921+L1108140808@users.noreply.github.com>
2022-05-02 23:49:57 +0800

    Create ex1-3.cpp

---
+++ ex1-3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello World ");
+    
+    cout <<  "hello chris ";
+    cout <<  "my height is " << 1 << "m " << 70 << "cm";
+
+    return 0;
+}
