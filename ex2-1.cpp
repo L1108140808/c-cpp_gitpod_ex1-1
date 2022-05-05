c611bc2e4dafc9b05f0f0c5bc2d08562b7512638
L1108140808 <104473921+L1108140808@users.noreply.github.com>
2022-05-05 12:35:17 +0800

    Create ex2-1.cpp

---
+++ ex2-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int      i;
+    double   f;
+    char     s;
+    
+    printf(" please input a integer \n");
+    scanf("%d", &i);
+    printf(" your input integer= %d\n",i);
+    
+    printf(" please input a float number \n");
+    scanf("%f",&f);
+    printf(" your input float number = %f\n",f);
+    
+    printf(" please input a string \n");
+    scanf("%s",s);
+    printf(" your input string = %s\n",s);
+    
+
+    return 0;
+}
