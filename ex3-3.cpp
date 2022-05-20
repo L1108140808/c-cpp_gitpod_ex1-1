73178f388e0627a934b85ba65ecfd1690594c7bd
L1108140808 <104473921+L1108140808@users.noreply.github.com>
2022-05-20 23:21:28 +0800

    Create ex3-3.cpp

---
+++ ex3-3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    printf("please input x: \n");
+    scanf("%d",&x);
+    
+    printf("please input y: \n");
+    scanf("%d",&y);
+    /*
+    + - * / %
+    */
+    
+    printf(" %d + %d = %d \n",x,y,x+y);
+    
+    printf("Hello World");
+
+    return 0;
+}
